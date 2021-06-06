f34ea1c548c6b2a21d4d98c31169b427b7625375
Gerardo <gr261212@gmail.com>
2021-06-06 17:20:19 +0000

    Nav scrolling effect

--- portfolio/src/main/webapp/script.js
+++ portfolio/src/main/webapp/script.js
@@ -1,31 +1,3 @@
-// Copyright 2020 Google LLC
-//
-// Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at
-//
-//     https://www.apache.org/licenses/LICENSE-2.0
-//
-// Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and
-// limitations under the License.
-
-/**
- * Adds a random greeting to the page.
- */
-// function addRandomGreeting() {
-//   const greetings =
-//       ["You talking to me?", 'Say "Hello" to my little friend', "Open the pod bay door, please, HAL", "Striker: surely you can't be serious", "Yo, Adrian", "My precious", "TETSUO!!"];
-
-//   // Pick a random greeting.
-//   const greeting = greetings[Math.floor(Math.random() * greetings.length)];
-
-//   // Add it to the page.
-//   const greetingContainer = document.getElementById('greeting-container');
-//   greetingContainer.innerText = greeting;
-// }
 const toggleNav = document.querySelector(".nav-toggle");
 const navMenu = document.querySelector(".navbar-menu");
 const navLinks = document.querySelectorAll(".nav-link");
@@ -45,4 +17,14 @@ navLinks.forEach((link) => {
     // hide nav menu
     navMenu.classList.remove("show-nav");
   });
+});
+
+// apply a util class to our nav once we reach a certain scroll threshold
+window.addEventListener("scroll", () => {
+  let nav = document.querySelector(".navbar");
+  if (window.scrollY > 50) {
+    nav.classList.add("navbar-scrolled");
+  } else {
+    nav.classList.remove("navbar-scrolled");
+  }
 });
