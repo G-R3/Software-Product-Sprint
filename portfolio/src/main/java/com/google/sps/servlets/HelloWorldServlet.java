6a63207a86be481394090032f92576c8c26b1792
Gerardo <gr261212@gmail.com>
2021-06-08 18:44:18 +0000

    Parse JSON to display random quote

--- portfolio/src/main/java/com/google/sps/servlets/HelloWorldServlet.java
+++ portfolio/src/main/java/com/google/sps/servlets/HelloWorldServlet.java
@@ -1,10 +1,14 @@
 package com.google.sps.servlets;
 
 import java.io.IOException;
+import java.util.ArrayList;
+
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+
+import com.google.gson.Gson;
 
 /** Handles requests sent to the /hello URL. Try running a server and navigating to /hello! */
 @WebServlet("/hello")
@@ -12,7 +16,11 @@ public class HelloWorldServlet extends HttpServlet {
 
   @Override
   public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
-    response.setContentType("text/html;");
-    response.getWriter().println("Hello Gerardo!");
+    String[] quotes = {"I'll be back","Houston, we have a problem", "Here's Johnny!" };
+
+    String json = new Gson().toJson(quotes);
+    
+    response.setContentType("application/json");
+    response.getWriter().println(json);
   }
 }
--- portfolio/src/main/webapp/message.js
+++ portfolio/src/main/webapp/message.js
@@ -1,9 +1,19 @@
+// container to display the quotes
 const messageContainer = document.getElementById("msg-container")
-async function getMessage() {
-    const response = await fetch("/hello");
-    const data = await response.text();
 
-    console.log(response);
-    console.log(data);
-    messageContainer.innerText = data;
+// fetches quotes from the server
+async function getQuote() {
+    const response = await fetch("/hello");
+    const data = await response.json();
+
+    // generate a random value between 0 - 2
+    const rand = Math.floor(Math.random() * 3);
+
+    console.log(data[rand])
+    messageContainer.innerText = data[rand];
 }
+
+// execute the getQuote function after the DOM is loaded
+document.addEventListener("DOMContentLoaded", () => {
+  getQuote();
+})
